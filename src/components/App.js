b9d8c1484a27ba1894304b93f99512eb963d7bf9
Anjali <anjaliguptajnp21@gmail.com>
2021-08-18 21:14:08 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,26 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-		super(props);
 
-		this.state = {
-			pHidden : true,
-		};
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { show: false };
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				<button id="click" 
-					onClick= {() => {
-						this.setState({pHidden: false});
-					}}
-				>Show Para</button>
-				{ this.state.pHidden ? null :( 
-					<p id="para">
-						Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-					</p>
-				)}
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{ /* Do not remove this main div!! */}
+				<button id="click" onClick={() => {
+					this.setState({ show: true });
+				}} >Render Paragraph</button>
+				{this.state.show && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				</div>
+		);
+	}
 }
+
 
 
 export default App;
